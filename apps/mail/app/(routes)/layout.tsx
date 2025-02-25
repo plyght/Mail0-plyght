e1e113b418bd919030cf1eca217bc06f2b62e961
plyght <nico.c.jaffer@gmail.com>
2025-02-25 08:24:23 -0500

    fix command palette showing twice

--- apps/mail/app/(routes)/layout.tsx
+++ apps/mail/app/(routes)/layout.tsx
@@ -6,18 +6,16 @@ import { SWRConfig } from "swr";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <CommandPaletteProvider>
-      <div className="flex h-screen w-screen overflow-hidden">
-        <SWRConfig
-          value={{
-            provider: typeof window !== "undefined" ? dexieStorageProvider : undefined,
-            keepPreviousData: true,
-            revalidateOnFocus: false,
-          }}
-        >
-          {children}
-        </SWRConfig>
-      </div>
-    </CommandPaletteProvider>
+    <div className="flex h-screen w-screen overflow-hidden">
+      <SWRConfig
+        value={{
+          provider: typeof window !== "undefined" ? dexieStorageProvider : undefined,
+          keepPreviousData: true,
+          revalidateOnFocus: false,
+        }}
+      >
+        <CommandPaletteProvider>{children}</CommandPaletteProvider>
+      </SWRConfig>
+    </div>
   );
 }
